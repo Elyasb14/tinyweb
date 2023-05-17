3fd4a6d112d9e3250265e81ff6906ab3335befae
Elyas Bianchi <114762087+Elyasb14@users.noreply.github.com>
2023-05-16 20:11:39 -0700

    fixed some bugs with the run script button

--- app.py
+++ app.py
@@ -9,18 +9,15 @@ def index():
     return render_template('index.html', the_title='tinyweather')
 
 
-# @app.route('/tinyweather/data')
-# def serve_csv():
-#     # Replace 'path_to_your_file.csv' with the actual path or URL of your CSV file
-#     return send_file('tinyweather/data/03-20-2023-rain.csv', mimetype='text/csv')
-
-
 @app.route('/', methods=['POST', 'GET'])
 @app.route('/data.html')
 def data():
     if request.method == 'POST':
-        script_output = subprocess.check_output(['python3', 'render_tinyweather_output.py'], universal_newlines=True)
-        return render_template('data.html', output=script_output, the_title="data")
+        try:
+            script_output = subprocess.check_output(['python3', 'render_tinyweather_output.py'], universal_newlines=True)
+            return render_template('data.html', output=script_output, the_title="data")
+        except Exception as e:
+            return render_template('data.html', output=f"Tinweather not responding, here is the error: {e}", the_title="data")
     return render_template('data.html', the_title="data")
     
 
@@ -36,4 +33,4 @@ def api_info():
     
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0")
+    app.run(debug=True, host="0.0.0.0", port=5001)
