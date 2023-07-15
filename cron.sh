2e2a11f982745f18b863b36a7efbb496470cf2f0
Elyasb14 <ebianchi@uoregon.edu>
2023-07-14 23:57:50 -0700

    120 seconds

--- cron.sh
+++ cron.sh
@@ -1,6 +1,7 @@
 crontab -r
+echo "All cronjobs removed successfully."
 crontab -l > crontab_new
 echo "* * * * * sleep 0s && python3 /home/ebianchi/tinyweb/tinyweather/main.py" >> crontab_new
-echo " * * * * * sleep 30s && python3 /home/ebianchi/tinyweb/tinyweather/main.py" >> crontab_new
+echo " * * * * * sleep 120s && python3 /home/ebianchi/tinyweb/tinyweather/main.py" >> crontab_new
 crontab crontab_new
 rm crontab_new
