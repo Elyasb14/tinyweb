b1d86942532beb9ed383cac279abc79227099043
Elyasb14 <ebianchi@uoregon.edu>
2023-08-22 21:52:24 -0700

    api dev

--- app.py
+++ app.py
@@ -1,30 +1,25 @@
 from flask import Flask, jsonify, render_template, request, send_file, redirect, url_for
-from flask_restful import Resource, Api
 from tinyweather.env import Rg15, Bme680
-#from tinyweather.gps import Gps
+from flask_restful import Resource, Api
 import os
 
 bme680 = Bme680()
 #rain = Rg15()
-#gps = Gps()
 
 app = Flask(__name__)
 api = Api(app)
 
 class RawData(Resource):
     def get(self):
-        global bme680
         bme680_dict = bme680.parse_data()
         # rain_dict = rain.parse_data()
         return jsonify(bme680_dict)
-
 
 @app.route('/')
 @app.route('/index.html')
 def index():
     """Render the index.html template with the title and GPS coordinates."""
     return render_template('index.html', the_title='tinyweather')
-                        # , lat=gps.parse_data()["lat"], lon=gps.parse_data()["lon"])
 
 @app.route('/')
 @app.route('/data.html')
@@ -38,16 +33,16 @@ def data():
         The rendered data.html template.
     """
     bme680_dict = bme680.parse_data()
-    rain_dict = rain.parse_data()
-    #gps_dict = gps.parse_data()
+    # rain_dict = rain.parse_data()
+
     return render_template('data.html',
                             temp=bme680_dict["temp (c)"],
                             pressure=bme680_dict["pressure"],
                             humid=bme680_dict["humidity"],
                             gas=bme680_dict["gas"],
-                            acc=rain_dict["Acc"],
-                            eventacc=rain_dict["EventAcc"],
-                            totalacc=rain_dict["TotalAcc"],
+                            # acc=rain_dict["Acc"],
+                            # eventacc=rain_dict["EventAcc"],
+                            # totalacc=rain_dict["TotalAcc"],
  #                           lat=gps_dict["lat"],
   #                          lon=gps_dict["lon"],
    #                         sattelites=gps_dict["num satelites"],
