82d6ce92d4b575d71ecada22c4a7bb8d040b35a9
Elyasb14 <ebianchi@uoregon.edu>
2023-08-30 18:58:19 -0700

    rain test!

--- cron.sh
+++ cron.sh
@@ -2,6 +2,6 @@ crontab -r
 echo "All cronjobs removed successfully."
 crontab -l > crontab_new
 echo "* * * * * sleep 0s && python3 /home/ebianchi/tinyweb/tinyweather/main.py" >> crontab_new
-echo " * * * * * sleep 120s && python3 /home/ebianchi/tinyweb/tinyweather/main.py" >> crontab_new
+echo " * * * * * sleep 300s && python3 /home/ebianchi/tinyweb/tinyweather/main.py" >> crontab_new
 crontab crontab_new
 rm crontab_new
