1f112d159ca5bf75a7c7e93385b4263485a3d342
Elyasb14 <ebianchi@uoregon.edu>
2023-10-25 10:57:23 -0700

    commit

--- tinyweather/main.py
+++ tinyweather/main.py
@@ -1,12 +1,8 @@
 from env import Rg15, Bme680
-from gps import Gps
 
     
-
 env = Bme680()
 rain = Rg15()
-gps = Gps()
 
 env.save_data(env.parse_data())
 rain.save_data(rain.parse_data())
-gps.save_data(gps.parse_data())
